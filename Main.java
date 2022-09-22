bb2cdbd31bd789efdb5a79e75ad7ecff3aa13816
AGURKANLEVENT <109485780+AGURKANLEVENT@users.noreply.github.com>
2022-09-23 00:54:28 +0300

    Yaricap Hesaplama Programi

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner dilim= new Scanner(System.in);
+        System.out.print("Dairenin Alanı, Çevre ve Dilim Alanı Hesaplama" +
+                "Dairenin yarıçapını giriniz :");
+        int r=dilim.nextInt();
+        System.out.print("Merkez açının ölçüsünü giriniz: ");
+        int a=dilim.nextInt();
+        double alan=Math.PI*r*r;
+        double cevre=2*Math.PI*r;
+        double dilimAlan=Math.PI*(r*r)*a/360;
+        System.out.println("Dairenin Alanı : "+alan);
+        System.out.println("Dairenin Çevresi : "+cevre);
+        System.out.println("Dairenin Diliminin Alanı : "+dilimAlan);
+
+    }
+}
